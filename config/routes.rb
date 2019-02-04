af7274031ebdf45399129d45aa0ac0b7608bab5e
nans64 <nans.noel@gmail.com>
2019-02-04 17:21:13 +0100

    Add the dynamic page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'welcome/first_name'
+  get 'static_pages/first_name'
+  # Welcome page
+  root 'static_pages#home'
+  # Other pages
+  get 'team', to: 'static_pages#team'
+  get 'contact', to: 'static_pages#contact'
+  # URL Dynamic
+  get '/welcome/:first_name', to: 'welcome#first_name'
 
 end
